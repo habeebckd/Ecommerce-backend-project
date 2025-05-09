3bffe8c051b8d7da3d45a54a8ce0476eb95d3f93
habeebckd <habee0483@gmail.com>
2025-05-09 12:20:16 +0530

    first

---
+++ ApiResponse/ApiResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace E_Commerce.ApiResponse
+{
+    public class ApiResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+        public string Error { get; set; }
+        public ApiResponse(bool isSuccess, string message, T data, string error)
+        {
+             IsSuccess = isSuccess;
+            Message = message;
+            Data = data;
+            Error = error;
+        }
+    }
+}
